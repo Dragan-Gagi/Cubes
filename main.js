465429da51c9c67931817c774184f6feaea172a1
Dragan-Gagi <57915558+Dragan-Gagi@users.noreply.github.com>
2019-11-29 12:20:00 +0100

    fixing header layout

---
+++ main.js
@@ -0,0 +1,57 @@
+
+
+ var firstName="Peter ";		
+ var has= "has ";
+ var dolars= 3500
+ var secondName = " Jim ";
+ var money = 5000
+ var value= "$";
+ var comma= ", ";
+ var and="and";
+
+
+
+ console.log (firstName + has + dolars + value + comma + and + secondName + has + money + value + comma + "print if " + firstName + has + "more money than" + secondName)
+ console.log (money > dolars)
+ console.log (money >= dolars)
+ console.log ( dolars> money)
+ console.log ( dolars==money)
+
+
+var thirdName= "John";
+var age= 38 ;
+var years= 8 ;
+var longSentence=" will be two times older than " ;
+var fourtName="Mike" ;
+var shortSentence= " How old is ";
+
+
+console.log (thirdName+ " is " + age + " years old. " + "In " + years + " years " + thirdName + longSentence + fourtName + "." + shortSentence + fourtName + " now?")
+
+console.log ( thirdName + " is " + (38+8) + " old") 
+console.log ( fourtName + " is " + (46 /2) + " old.")
+
+var animal= " birds";
+var firstGroup= " first flock ";
+var secondGroup=" second flock ";
+var firstFlock= 78;
+var secondFlock= 57;
+var thirdNumber= 12;
+var fourtNumber= 4;
+var a= " in a"
+var question= "Is the first flock larger or equal to the second one ?"
+var sentence = "decied to move from the"
+
+
+
+console.log( "There are " + firstFlock + animal + a + firstGroup + "and " + secondFlock + a + secondGroup + "."  + thirdNumber +  sentence  + firstGroup + "to the " + secondGroup + ", and then " + fourtNumber + animal + " left" + secondGroup + ". "  + question )
+
+console.log ( firstGroup= firstFlock - thirdNumber )
+console.log ( secondGroup= secondFlock + thirdNumber )
+console.log  (secondGroup - fourtNumber)
+console.log( "First group haves more birds then second group" )
+console.log( firstGroup>secondGroup)
+console.log ( "First and second group are not equal,because the first group haves one bird more then the second")
+console.log (firstGroup==secondGroup)
+console.log ("first group haves more then second group and they are not equal")
+console.log( firstGroup>=secondGroup)
